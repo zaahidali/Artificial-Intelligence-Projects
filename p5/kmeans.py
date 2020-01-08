35a1bd115ee1068e16b4f2207c476173a9d96a0f
Zahid Ali <37751705+zaid4317@users.noreply.github.com>
2020-01-08 10:38:43 +0300

    Update kmeans.py

--- p5/kmeans.py
+++ p5/kmeans.py
@@ -1,91 +1,140 @@
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
-import pandas as pd
-import numpy as np
-import copy
+import math as take, itertools as running
+from collections import defaultdict as dictionary
+from random import uniform as same
+import matplotlib.pyplot as draw
 
-############################## This program is written in Python using PyCharm ################################
-x = []
-y = []
-pdf = PdfPages('plot.pdf')
-with open('data.txt', 'r') as f:
-    next(f)
-    for line in f:
-        line = line.split(',')
-        x.extend([int(line[0])])
-        y.extend([int(line[1])])
+def DisplayAndShowThePlot():
+        clusters = 5
+        plotGraph = draw.figure()
+        out = list()
+        data = MainProgram(SetData, clusters)
+        for i, j in zip(SetData, data):
+            out.append((i, j))
+        result = sorted(out, key=lambda x: x[1])
+        listOfValues = [list(v) for k, v in running.groupby(result, lambda x: x[1])]
+        a0,a1 = 0,1
+        for i, color in zip(listOfValues, colors):
+            x = CalculateXYaxisValues(i)[a0]
+            y = CalculateXYaxisValues(i)[a1]
+            draw.scatter(x, y,  color=color,marker="*", s=50)
+        draw.title('For K = ' + "5")
+        plotGraph.savefig("plot.pdf", bbox_inches='tight')
+#///////////////////////////////////////////////////////////////
 
+green = 'green'
+red = 'red'
+blue = 'blue'
+brown = 'brown'
+pink = 'pink'
+colors = [green, red, pink, blue, brown]
 
-df = pd.DataFrame({
-    'x': x,
-    'y': y
-})
-#########################################################################################
-minx = min(x)
-miny = min(y)
-maxx = max(x)
-maxy = max(y)
-colmap = {1: 'b', 2: 'g', 3: 'r', 4: 'c', 5: 'm'}
+'//////////////////////////////////////////////////////////////'
+SetData = list()
+def ReadDataFromFile():
+    f1 = 1
+    comma = ','
+    filesData = open('data.txt', 'r')
+    lines = filesData.read().splitlines()
+    for element in lines[f1:]:
+        item = [int(i.strip()) for i in element.split(comma)]
+        SetData.append(item)
+'//////////////////////////////////////////////////////////////'
+ReadDataFromFile()
 
-for k in range(2, 6):
-    centroids = {
-        i + 1: [np.random.randint(minx, maxx), np.random.randint(miny, maxy)]
-        for i in range(k)
-    }
-
-
-    def assignment(df, centroids):
-        for i in centroids.keys():
-            df['distance_from_{}'.format(i)] = (
-                np.sqrt(
-                    (df['x'] - centroids[i][0]) ** 2
-                    + (df['y'] - centroids[i][1]) ** 2
-                )
-            )
-        centroid_distance_cols = ['distance_from_{}'.format(i) for i in centroids.keys()]
-        df['closest'] = df.loc[:, centroid_distance_cols].idxmin(axis=1)
-        df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
-        df['color'] = df['closest'].map(lambda x: colmap[x])
-        return df
-
-
-    #########################################################################################
-
-    df = assignment(df, centroids)
-    print(df.head())
-
-    #########################################################################################
-    old_centroids = copy.deepcopy(centroids)
+def CalculateXYaxisValues(SetData):
+    x_axis,y_axis = list(), list()
+    a0,a1 = 0,1
+    for i in SetData:
+        x_axis.append(i[a0][a0])
+        y_axis.append(i[a0][a1])
+    return [x_axis, y_axis]
+#///////////////////////////////////////////////////////////////
 
 
-    def update(k):
-        for i in centroids.keys():
-            centroids[i][0] = np.mean(df[df['closest'] == i]['x'])
-            centroids[i][1] = np.mean(df[df['closest'] == i]['y'])
-        return k
+ForMinValues = 'min_%d'
+ForMaxValues = 'max_%d'
+def setCentroids(data_set, k):
+    cpoint = list()
+    v0 = 0
+    axis = len(data_set[v0])
+    XYminmaxValues = dictionary(int)
+    for point in data_set:
+        for i in range(axis):
+            val = point[i]
+            ForMinV = ForMinValues % i
+            ForMaxV = ForMaxValues % i
+            if val > XYminmaxValues[ForMaxV] or ForMaxV not in XYminmaxValues:
+                XYminmaxValues[ForMaxV] = val
+            elif val < XYminmaxValues[ForMinV] or ForMinV not in XYminmaxValues :
+                XYminmaxValues[ForMinV] = val
+            else:
+                "Do nothing"
+    for i in range(k):
+        xymeans = list()
+        for j in range(axis):
+            min_val = XYminmaxValues[ForMinValues % j]
+            max_val = XYminmaxValues[ForMaxValues % j]
+            xymeans.append(same(min_val, max_val))
+        cpoint.append(xymeans)
+    return cpoint
 
 
-    #########################################################################################
-    centroids = update(centroids)
+def Meanmeans(means):
+    dimension_number = len(means[0])
+    center = list()
+    for dimension in range(dimension_number):
+        total = 0
+        for point in means:
+            total = total + point[dimension]
+        center.append(total / float(len(means)))
+    return center
 
-    df = assignment(df, centroids)
 
-    while True:
-        closest_centroids = df['closest'].copy(deep=True)
-        centroids = update(centroids)
-        df = assignment(df, centroids)
-        if closest_centroids.equals(df['closest']):
-            break
-    #########################################################################################
-    fig = plt.figure(figsize=(5, 5))
-    plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
-    for i in centroids.keys():
-        plt.scatter(*centroids[i], color=colmap[i])
-    plt.xlim(minx, maxx)
-    plt.ylim(miny, maxy)
-    plt.title('k = ' + str(k))
-    # plt.show()
-    # fig.savefig("plot.pdf")
-    pdf.savefig(fig)
-    plt.close()
-pdf.close()
+def update(data_set, data_setting):
+    new_means = dictionary(list)
+    centroids_P = list()
+    for res, point in zip(data_setting, data_set):
+        new_means[res].append(point)
+    for means in new_means.values():
+        centroids_P.append(Meanmeans(means))
+    return centroids_P
+
+#/////////////////////////////////////////////
+def calculate_distance(x2,x1):
+    res = 0
+    for values in range(len(x2) - 1):
+        res = res+  pow((x2[values] - x1[values]), 2)
+    return take.sqrt(res)
+#////////////////////////////////////////////
+
+def XYmeans(data_means, cpoint):
+    measure = list()
+    for point in data_means:
+        s = take.inf
+        first = 0
+        for i in range(len(cpoint)):
+            val = calculate_distance(point, cpoint[i])
+            if val < s:
+                s = val
+                first = i
+        measure.append(first)
+    return measure
+
+
+def MainProgram(SetData, k):
+    old_Values = None
+    cpoint = setCentroids(SetData, k)
+    Values = XYmeans(SetData, cpoint)
+    final_cpoint = list()
+    while Values != old_Values:
+        new_cpoint = update(SetData, Values)
+        final_cpoint = new_cpoint
+        old_Values = Values
+        Values = XYmeans(SetData, new_cpoint)
+    for i, color in zip(final_cpoint, colors):
+        draw.scatter(i[0], i[1], s=100)
+    return Values
+
+
+
+DisplayAndShowThePlot()
